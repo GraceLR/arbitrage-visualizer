1545c2d54610cd4f899074b9f1b5afe0bf1ff627
GraceLR <gracerwork08@gmail.com>
2022-06-19 15:28:39 -0400

    successful flow from db and use class on db_operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-    // const [apiStatus, setApiStatus] = useState(null);
-
-    // const getStatus = () =>
-    //     axios.get('http://localhost:6060/status').then((res) => {
-    //         console.log('#####', 'trigdered');
-    //         setApiStatus(res.data.message);
-    //     });
-
-    let draft = undefined;
-    axios.get('http://localhost:8080').then((res) => {
-        console.log('axios.get triggered###########');
-        draft = res;
-        console.log(draft, 'draft###########');
-    });
-
-    return <div className="App">{draft}</div>;
+    const [apiStatus, setApiStatus] = useState<any>(null);
+    useEffect(() => {
+        axios.get("/api")
+        .then(all => {
+            setApiStatus((_prev:any) => all.data.names);
+        });
+      }, []);
+    return <div className="App">{apiStatus}</div>;
 }
 
 export default App;
