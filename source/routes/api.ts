3fdde7af5c7aea206d507c9a053120ba1e6834b1
GraceLR <gracerwork08@gmail.com>
2022-07-01 18:58:02 -0400

    add types for /api axios get call

--- source/routes/api.ts
+++ source/routes/api.ts
@@ -1,4 +1,5 @@
 import db_operations from "../db/db_operations";
+import { Arb } from "../types/types";
 
 const router = require("express").Router();
 
@@ -6,7 +7,7 @@ module.exports = () => {
 
   router.get("/", async (req: any, res: any) => {
     const data = await db_operations.db_export();
-    res.send(data);
+    res.send(data.rows.map(a => ({ id: a[0], block: a[1], created_on: a[2], profit: a[3]})) as Arb[]);
   });
 
 
