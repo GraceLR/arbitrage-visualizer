f4e5bab7edc4c1db08df9cdd0634968911d4bde3
GraceLR <gracerwork08@gmail.com>
2022-07-04 15:32:21 -0400

    asend [] of Node object, alter column name from node to node_name in table cycles

--- source/routes/api.ts
+++ source/routes/api.ts
@@ -5,12 +5,12 @@ module.exports = () => {
 
   router.get("/arbs", async (req: any, res: any) => {
     const data = await db_operations.db_export_arbs();
-    res.send(data.rows.map(a => ({ id: a[0], block: a[1], created_on: a[2], profit: a[3]})));
+    res.send(data.rows.map(arb => ({ id: arb[0], block: arb[1], created_on: arb[2], profit: arb[3] })));
   });
 
   router.get("/arbs/:id", async (req: any, res: any) => {
     const data = await db_operations.db_export_cycle(req.params.id);
-    res.send(data);
+    res.send(data.rows.map(node => ({ id: node[0], node_name: node[1], position: node[2], arb_id : node[3] })));
   });
 
   return router;
